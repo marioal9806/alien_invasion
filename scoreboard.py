ae7bc54258ba71b6e4a5cbe7f6c8f0ae904929c2
Mario Ortega <marioal9806@hotmail.com>
2020-01-29 14:13:30 -0600

    Refactor scoreboard __init__()

--- scoreboard.py
+++ scoreboard.py
@@ -18,12 +18,15 @@ class Scoreboard:
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 36)
 
+        self.prep_images()
+    
+    def prep_images(self):
         # Prepare the initial score images.
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
         self.prep_ships()
-    
+
     def prep_score(self):
         """Turn the score into a rendered image."""
         rounded_score = round(self.stats.score, -1)
