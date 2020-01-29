ed1a47d3e88379b4b547484a4e8fa1c42eb56ea1
Mario Ortega <marioal9806@hotmail.com>
2020-01-29 14:11:43 -0600

    Refactor _check_bullet_alien_collisions()

--- alien_invasion.py
+++ alien_invasion.py
@@ -182,16 +182,17 @@ class AlienInvasion:
             self.sb.check_high_score()
 
         if not self.aliens:
-            # Destroy existing bullets and create new fleet.
-            self.bullets.empty()
-            self._create_fleet()
-            self.settings.increase_speed()
-            
-            # Increase level.
-            self.stats.level += 1
-            self.sb.prep_level()
+            self._start_new_level()
 
-
+    def _start_new_level(self):
+        # Destroy existing bullets and create new fleet.
+        self.bullets.empty()
+        self._create_fleet()
+        self.settings.increase_speed()
+        
+        # Increase level.
+        self.stats.level += 1
+        self.sb.prep_level()
 
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screen."""
