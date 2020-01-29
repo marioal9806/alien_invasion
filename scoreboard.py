a8ef3d99a3b117926cd34e135569996112ea5484
Mario Ortega <marioal9806@hotmail.com>
2020-01-29 14:07:34 -0600

    Undate high score label

--- scoreboard.py
+++ scoreboard.py
@@ -46,7 +46,7 @@ class Scoreboard:
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
         high_score = round(self.stats.high_score, -1)
-        high_score_str = "{:,}".format(high_score)
+        high_score_str = "High Score: {:,}".format(high_score)
         self.high_score_image = self.font.render(high_score_str, True,
             self.text_color, self.settings.bg_color)
 
